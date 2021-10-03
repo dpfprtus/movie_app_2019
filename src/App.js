396cbe7ae3bfb269127cf5552922f47cecacd0a2
dpfprtus <dpfprtus@gmail.com>
2021-10-03 08:13:15 +0000

    practice CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import Movie from './Movie.js';
+import "./App.css";
 //import PropTypes from "prop-types";
 
 class App extends React.Component{
@@ -9,16 +11,37 @@ class App extends React.Component{
 	
 	getMovies = async()=> {
 		// eslint-disable-next-line
-		const movies = await  axios.get("https://yts.mx/api/v2/list_movies.json");
+		const {data:{data:{movies}}} = await  axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+		this.setState({movies,isLoading:false});
 	}
-	async componentDidMount(){
+	componentDidMount(){
   
     	this.getMovies();
 	};
 	
 	render(){
-		const { isLoading} = this.state;
-		return <div>{isLoading ? "isLoading" : "We r ready."}</div>;
+		const { isLoading, movies} = this.state;
+		return (
+			<section className="container">{isLoading 
+					? <div className="loader">
+						<span className="loader_text">Loading</span>
+										</div>
+				    : (<div className="movies">
+						   {movies.map(movie=>(
+							   <Movie 
+								   key={movie.id}
+								   id={movie.id}
+								   year={movie.year}
+								   summary={movie.summary}
+								   title={movie.title}
+								   poster={movie.medium_cover_image}
+								   genres={movie.genres}
+								   />
+						   ))}
+					   </div>
+					)
+						
+			}</section>);
 	}
 		}
 export default App;
