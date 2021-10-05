fc954ce164c1897c6643c4f9cda24a03913ac16f
dpfprtus <dpfprtus@gmail.com>
2021-10-05 07:26:50 +0000

    learn React Route

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie.js';
-import "./App.css";
-//import PropTypes from "prop-types";
+import React from "react";
+import {HashRouter,Route} from "react-router-dom";
+// eslint-disable-next-line
+import About from "./routes/about";
+// eslint-disable-next-line
+import Home from "./routes/home";
 
-class App extends React.Component{
-	state = {
-		isLoading: true
-	};
-	
-	getMovies = async()=> {
-		// eslint-disable-next-line
-		const {data:{data:{movies}}} = await  axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
-		this.setState({movies,isLoading:false});
-	}
-	componentDidMount(){
-  
-    	this.getMovies();
-	};
-	
-	render(){
-		const { isLoading, movies} = this.state;
-		return (
-			<section className="container">{isLoading 
-					? <div className="loader">
-						<span className="loader_text">Loading</span>
-										</div>
-				    : (<div className="movies">
-						   {movies.map(movie=>(
-							   <Movie 
-								   key={movie.id}
-								   id={movie.id}
-								   year={movie.year}
-								   summary={movie.summary}
-								   title={movie.title}
-								   poster={movie.medium_cover_image}
-								   genres={movie.genres}
-								   />
-						   ))}
-					   </div>
-					)
-						
-			}</section>);
-	}
-		}
+function App(){
+	return (
+		<HashRouter>
+		<Route path="/" exact={true} component={Home} />
+		
+		<Route path="/about" component={About}/>
+		
+	</HashRouter>
+		);
+}
+
 export default App;
